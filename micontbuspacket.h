421f73260e5a23a8937f6276e990061064302f98
Borealis Technologies <borealis@permlug.org>
2015-09-02 11:23:16 +0500

    add all other cmd & result codes

--- micontbuspacket.h
+++ micontbuspacket.h
@@ -10,13 +10,26 @@ class MicontBusPacket
 public:
 
     enum CmdCode {
-        CMD_GETSIZE = 0x01,
-        CMD_GETBUF_B = 0x02,
-        CMD_PUTBUF_B = 0x04
+        CMD_GETSIZE =   0x01,
+        CMD_GETBUF_B =  0x02,
+        CMD_GETBUF =    0x03,
+        CMD_PUTBUF_B =  0x04,
+        CMD_PUTBUF =    0x05,
     };
 
     enum ResultCode {
-        CMD_RESULT_OK = 0x10,
+        CMD_RESULT_OK =         0x10,
+        CMD_RESULT_WAIT =       0x20,
+        CMD_RESULT_BUSY =       0x30,
+        CMD_RESULT_UCMD =       0x40,
+        CMD_RESULT_ERRVAR =     0x50,
+        CMD_RESULT_ERRCMD =     0x60,
+        CMD_RESULT_ERRARG =     0x70,
+        CMD_RESULT_ERRBSIZE =   0x80,
+        CMD_RESULT_ERRBADDR =   0x90,
+        CMD_RESULT_ERRPWD =     0xa0,
+        CMD_RESULT_ERRLFT =     0xb0,
+        CMD_RESULT_ERRDONE =    0xc0,
     };
 
     MicontBusPacket();
